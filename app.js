b77abb3af0e104db173749b8215fae4c55099997
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-09 19:07:26 +0300

    +IV guard

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ import genHash from './src/util/genHash';
 import User from './src/user/User';
 
 config.printAll();
-console.log(process.env.NODE_ENV);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+if (process.env.INIT_VECTOR === undefined) {
+    console.error("[Error]: Environment variable 'INIT_VECTOR' not set");
+    process.exit(1);
+}
 
 const expressApp = express();
 const userManager = new UserManager(expressApp, config);
