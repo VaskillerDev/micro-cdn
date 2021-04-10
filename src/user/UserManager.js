3ec760a646b981d17be8bf1ba3f80bd5cc00bed9
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-11 02:26:47 +0300

    +getVerifyMiddleware() - validation via jwt feature

--- src/user/UserManager.js
+++ src/user/UserManager.js
@@ -1,4 +1,6 @@
-﻿import express from 'express';
+﻿'use strict';
+
+import express from 'express';
 import ConfigBase from '../../config/config.base';
 import bodyParser from 'body-parser';
 import { v4 as uuidv4 } from 'uuid';
@@ -6,9 +8,15 @@ import genHash from '../util/genHash';
 import mongodb from 'mongodb';
 import User from './User';
 import isUser from './isUser';
+import Base64ToObject from '../util/Base64ToObject';
+import Utf8ToBase64Url from '../util/Utf8ToBase64Url';
+import getHmac from '../util/getHmac';
 
 const USER_ALREADY_SIGNUP = { token: null, message: 'user already sign up' };
 const USER_SIGNIN_FAILED = { token: null, message: 'sign in is failed' };
+const USER_UNAUTHORIZED = { message: 'unauthorized' };
+const INVALID_TOKEN = { message: 'invalid token' };
+const TARGET_COLLECTION = 'users';
 
 /**
  * @property {e.Express} this._expressApp
@@ -24,16 +32,14 @@ class UserManager {
     this._config = config;
 
     this.#setSettings();
-    this.#setListeners();
   }
 
   #setSettings = () => {
     this._expressApp.use(bodyParser.urlencoded({ extended: false }));
     this._expressApp.use(bodyParser.json());
-    this._mongoClient = new mongodb.MongoClient(this._config.MONGO_URL);
   };
 
-  #setListeners = () => {
+  listen = () => {
     /* {
       "userData": {
       "name": "MyName",
@@ -72,11 +78,9 @@ class UserManager {
       const userData = req.body.userData;
       const name = userData.name;
       const email = userData.email;
-
-      const user = new User(null, name, email, null, false);
       const filter = { _name: name, _email: email };
 
-      this.tryFetchFromStorage(user, filter, this.#searchUser)
+      this.tryFetchFromStorage(filter, this.#searchUser)
         .then(maybeUser => User.createFromObject(maybeUser)?.toJwtSync())
         .then(maybeToken =>
           maybeToken
@@ -85,6 +89,67 @@ class UserManager {
         );
     });
   };
+  /* {
+    "jwt": "eyJhbGci.VzZXIiLCJlbWFpbCI6ImV4YW1wbGVNYWlsQG1.7B/c+a/6eVp4WHUMEr"
+   } */
+  getVerifyMiddleware() {
+    return (req, res, next) => {
+      const jwt = req.headers['authorization'];
+      // const authHeader = req.headers['Authorization']: Bearer <token>;
+
+      if (jwt === null || jwt === undefined) {
+        res.status(422).send(INVALID_TOKEN);
+        return;
+      }
+
+      const [headerEncode, payloadEncode, secretEncode] = jwt.split('.');
+      const isValidEncodeFields = !!headerEncode && !!payloadEncode && !!secretEncode;
+      if (!isValidEncodeFields) {
+        res.status(422).send(INVALID_TOKEN);
+        return;
+      }
+
+      const header = Base64ToObject(headerEncode);
+      const payload = Base64ToObject(payloadEncode);
+      const secret = secretEncode;
+
+      const name = payload?.name;
+      const email = payload?.email;
+      const uuid = payload?.uuid;
+      const isValidDecodeFieldsInPayload = !!uuid && !!name && !!email;
+
+      if (!isValidDecodeFieldsInPayload) {
+        res.status(422).send(INVALID_TOKEN);
+        return;
+      }
+
+      const filter = { _name: name, _email: email, _uuid: uuid };
+
+      this.tryFetchFromStorage(filter, this.#searchUser)
+        .then(User.createFromObject)
+        .then(user => {
+          if (user?.getUuid() === uuid) {
+            // is user
+
+            const headerEncode = Utf8ToBase64Url(JSON.stringify(header));
+            const payloadEncode = Utf8ToBase64Url(JSON.stringify(payload));
+            const rawToken = headerEncode + '.' + payloadEncode;
+
+            const hmac = getHmac(user._hash);
+            let secretEncode = hmac.update(rawToken).digest('hex');
+            secretEncode = Utf8ToBase64Url(secretEncode);
+
+            if (secretEncode !== secret) {
+              res.status(422).send(INVALID_TOKEN);
+              return;
+            }
+
+            next();
+          } else res.status(401).send(USER_UNAUTHORIZED);
+        })
+        .catch(err => console.log(err));
+    };
+  }
 
   // trying push data if cell in collection is free
   tryPushToStorage(user) {
@@ -93,15 +158,15 @@ class UserManager {
     const filter = { _email: email };
 
     return new Promise(resolve =>
-      this.tryFetchFromStorage(user, filter, this.#searchUser).then(maybeUser =>
+      this.tryFetchFromStorage(filter, this.#searchUser).then(maybeUser =>
         maybeUser ? resolve(null) : this.pushToStorage(user, result => resolve(result))
       )
     );
   }
-  
-  #searchUser(user, filter, cb) {
+
+  #searchUser(filter, cb) {
     const db = this._mongoClient.db(this._config.MONGO_DB_NAME);
-    const userCollection = db.collection('users');
+    const userCollection = db.collection(TARGET_COLLECTION);
 
     let maybeUser = UserManager.#getUserFromCollection(userCollection, filter);
     cb(maybeUser);
@@ -117,8 +182,9 @@ class UserManager {
     if (user === null) return;
     this._mongoClient = new mongodb.MongoClient(this._config.MONGO_URL);
     this._mongoClient.connect(err => {
+      if (err != null) console.log(err);
       const db = this._mongoClient.db(this._config.MONGO_DB_NAME);
-      const usersCollection = db.collection('users');
+      const usersCollection = db.collection(TARGET_COLLECTION);
 
       usersCollection.insertOne(user, (err, res) => {
         if (err != null) console.log(err);
@@ -129,12 +195,12 @@ class UserManager {
     });
   }
 
-  tryFetchFromStorage(user, filter, searchFunction) {
+  tryFetchFromStorage(filter, searchFunction) {
     // (User) => Promise(User?)
     this._mongoClient = new mongodb.MongoClient(this._config.MONGO_URL);
     return new Promise(resolve => {
       this._mongoClient.connect(
-        searchFunction.bind(this, user, filter, maybeUser => resolve(maybeUser))
+        searchFunction.bind(this, filter, maybeUser => resolve(maybeUser))
       );
       this._mongoClient.close();
     });
