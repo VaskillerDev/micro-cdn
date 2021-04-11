0d2cecd3849638e96bb9aab988e07e086dde0fb2
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-11 03:35:57 +0300

    rm

--- test/create-user.test.js
+++ test/create-user.test.js
@@ -3,10 +3,6 @@ import isUser from '../src/user/isUser';
 import User from '../src/user/User';
 import { v4 as uuidv4 } from 'uuid';
 import * as assert from 'assert';
-import express from 'express';
-import UserManager from '../src/user/UserManager';
-import config from '../config';
-import VfsManager from '../src/vfs/VfsManager';
 
 {
   const uuid = uuidv4();
@@ -18,15 +14,6 @@ import VfsManager from '../src/vfs/VfsManager';
   const user = new User(uuid, name, email, hash, isActivate);
 
   assert.strictEqual(isUser(user), true);
-
-  config.printAll();
-  console.log('NODE_ENV:', process.env.NODE_ENV);
-
-  const expressApp = express();
-  const vfsManager = new VfsManager(expressApp, config);
-  const userManager = new UserManager(expressApp, config);
-
-  expressApp.listen(3000);
 }
 
 {
