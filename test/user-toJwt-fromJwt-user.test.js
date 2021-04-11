795aef924dfd726652dcb05c81018b6ecc608fdd
VaskillerDev <Vaskoooo9241@gmail.com>
2021-04-11 04:03:37 +0300

    +tests, npm run test

--- test/user-toJwt-fromJwt-user.test.js
+++ test/user-toJwt-fromJwt-user.test.js
@@ -16,12 +16,16 @@ import isUser from '../src/user/isUser';
   assert.strictEqual(isUser(user), true);
 
   (async () => {
-    const jwt = await user.toJwt();
-    const userFromJwt = await User.createFromJwt(jwt);
+    try {
+      const jwt = await user.toJwt();
+      const userFromJwt = await User.createFromJwt(jwt);
 
-    assert.strictEqual(userFromJwt.getName() === name, true);
-    assert.strictEqual(userFromJwt.getEmail() === email, true);
-    assert.strictEqual(userFromJwt.getUuid() === uuid, true);
-    assert.strictEqual(isUser(userFromJwt), false); // it is not complete User instance - missing fields
+      assert.strictEqual(userFromJwt.getName() === name, true);
+      assert.strictEqual(userFromJwt.getEmail() === email, true);
+      assert.strictEqual(userFromJwt.getUuid() === uuid, true);
+      assert.strictEqual(isUser(userFromJwt), false); // it is not complete User instance - missing fields
+    } catch (e) {
+      console.error(e);
+    }
   })();
 }
---
+++ tests.js
@@ -0,0 +1,5 @@
+﻿'use strict'
+// --es-module-specifier-resolution=node
+
+import './test/create-user.test';
+import './test/user-toJwt-fromJwt-user.test';
